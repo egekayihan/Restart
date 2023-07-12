007a01e926388c117751d40cf38ee94918e0d9dc
ek223gi <ek223gi@student.lnu.se>
2023-07-12 18:58:37 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ege Kayihan on 6.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
